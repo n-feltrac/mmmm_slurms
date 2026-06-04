7e58042a09e3d62748cf2283c6e4d6b12c771bb7
go away <shutup@git.hub>
2026-06-04 18:03:46 +0930

    mfw i comment out ONE (1) incredibly important line:

--- Room.cpp
+++ Room.cpp
@@ -13,28 +13,34 @@ void Room::update(RenderWindow &window) {
 void Room::parseRoom(string room, string tileset) {
     ifstream rm(room, ios::in);
     string st;
-    rm >> st;
+    // rm >> st;
     // cout << st << endl;
     string current = "";
     int posX = 0;
     int posY = 0;
-    for(int i = 0; i < st.length(); i++) {
-        if(st[i] == ',') {
-            // cout << i*16 << endl;
-            Tile* t = new Tile(tileset, stoi(current), posX * 16, posY * 16);
-            tiles.push_back(t);
-            // cout << "comma!" << endl;
-            current = "";
-            // i++;
-            posX++;
-            if(posX >= 16) { // should replace with roomLength for longer rooms
-                posX = 0;
-                posY++;
+    while(std::getline(rm, st)) {
+        for(int i = 0; i < st.length(); i++) {
+            if(st[i] == ',') {
+                cout << current << endl;
+                Tile* t = new Tile(tileset, stoi(current), posX * 16, posY * 16);
+                tiles.push_back(t);
+                cout << posX << " and " << posY << endl;
+                current = "";
+                // i++;
+                posX++;
+                // if(posX >= 16) { // should replace with roomLength for longer rooms
+                //     posX = 0;
+                // }
+            } else {
+                current += st[i];
+                // cout << current << endl;
             }
-        } else {
-            current += st[i];
-            // cout << current << endl;
         }
+        cout << "end of line!" << endl;
+        posX = 0;
+        posY++;
     }
+    
+
     // CURSED DO NOT USE!!!!!!!!!!!!!!
 }
